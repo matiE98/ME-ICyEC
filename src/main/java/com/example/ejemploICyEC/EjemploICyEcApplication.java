3b9df336a484f98cf26ae54708981dddd1e06e97
Evin Matias <matiasevin98@gmail.com>
2021-06-29 16:30:36 -0300

    probando hola mundo

---
+++ src/main/java/com/example/ejemploICyEC/EjemploICyEcApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejemploICyEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploICyEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploICyEcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/ejemploICyEC/controller/HolaMundo.java
@@ -0,0 +1,15 @@
+package com.example.ejemploICyEC.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HolaMundo {
+	@RequestMapping(method=RequestMethod.GET)
+	public String home() {
+		return "Hola mundo";
+	}
+	
+}
